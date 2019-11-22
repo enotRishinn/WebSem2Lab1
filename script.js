eee2c2b4cbd7eae6d8e416561cc0c8afae1cc564
= <=>
2019-11-22 09:46:31 +0300

    test

--- script.js
+++ script.js
@@ -6,17 +6,20 @@ export function onSubmit(e) {
 }
 
 export function fetchWeather(city){
+  let source = document.getElementById("weather-entry-template").innerHTML;
+  let error_source = document.getElementById("error-entry-template").innerHTML;
+  let weather_block = document.getElementById("weather-block").innerHTML;
+  let error_block = document.getElementById("error-block").innerHTML;
+  
   return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=665c8e98e586f364800fd238b845d042&units=metric&lang=en`)
     .then(response => { response.json()
       .then(json => {
-        var source = document.getElementById("weather-entry-template").innerHTML;
-        var error_source = document.getElementById("error-entry-template").innerHTML;
         if (response.ok) {
-          document.getElementById("weather-block").innerHTML = renderBody(getWeather(json), source);
-          document.getElementById("error-block").innerHTML = renderBody(null, error_source);
+          weather_block = renderBody(getWeather(json), source);
+          error_block = renderBody(null, error_source);
         } else {
-          document.getElementById("weather-block").innerHTML = renderBody(null, source);
-          document.getElementById("error-block").innerHTML = renderBody(getError(json), error_source);
+          weather_block = renderBody(null, source);
+          error_block = renderBody(getError(json), error_source);
         }
       });
     },
